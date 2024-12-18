4dacaa89ffd5c4045b3f257087b7d01ad4ee3e6c
Grzegorz Bokota <bokota+github@gmail.com>
2024-12-18 19:13:35 +0100

    initial add rust code

---
+++ crates/triangulation/src/lib.rs
@@ -0,0 +1,44 @@
+mod path_triangulation;
+mod point;
+
+use numpy::ndarray::{ArrayD, ArrayViewD, ArrayViewMutD};
+use numpy::{IntoPyArray, PyArrayDyn, PyArrayMethods, PyReadonlyArrayDyn};
+use pyo3::{pymodule, types::PyModule, Bound, PyResult, Python};
+
+#[pymodule]
+fn rust_ext<'py>(_py: Python<'py>, m: &Bound<'py, PyModule>) -> PyResult<()> {
+    // example using immutable borrows producing a new array
+    fn axpy(a: f64, x: ArrayViewD<'_, f64>, y: ArrayViewD<'_, f64>) -> ArrayD<f64> {
+        a * &x + &y
+    }
+
+    // example using a mutable borrow to modify an array in-place
+    fn mult(a: f64, mut x: ArrayViewMutD<'_, f64>) {
+        x *= a;
+    }
+
+    // wrapper of `axpy`
+    #[pyfn(m)]
+    #[pyo3(name = "axpy")]
+    fn axpy_py<'py>(
+        py: Python<'py>,
+        a: f64,
+        x: PyReadonlyArrayDyn<'py, f64>,
+        y: PyReadonlyArrayDyn<'py, f64>,
+    ) -> Bound<'py, PyArrayDyn<f64>> {
+        let x = x.as_array();
+        let y = y.as_array();
+        let z = axpy(a, x, y);
+        z.into_pyarray(py)
+    }
+
+    // wrapper of `mult`
+    #[pyfn(m)]
+    #[pyo3(name = "mult")]
+    fn mult_py<'py>(a: f64, x: &Bound<'py, PyArrayDyn<f64>>) {
+        let x = unsafe { x.as_array_mut() };
+        mult(a, x);
+    }
+
+    Ok(())
+}
---
+++ crates/triangulation/src/point.rs
@@ -0,0 +1,280 @@
+pub mod triangulation {
+    pub mod point {
+        use std::cmp::Ordering;
+        use std::fmt;
+        use std::hash::{Hash, Hasher};
+
+        pub(crate) type Coord = f32;
+        #[derive(Debug, Clone, Copy)]
+        pub struct Point {
+            pub x: Coord,
+            pub y: Coord,
+        }
+
+        impl Point {
+            pub fn new(x: Coord, y: Coord) -> Self {
+                Self { x, y }
+            }
+
+            pub fn add(&self, other: &Point) -> Vector {
+                Vector {
+                    x: self.x + other.x,
+                    y: self.y + other.y,
+                }
+            }
+
+            pub fn sub(&self, other: &Point) -> Vector {
+                Vector {
+                    x: self.x - other.x,
+                    y: self.y - other.y,
+                }
+            }
+
+            pub fn add_vector(&self, vector: &Vector) -> Point {
+                Point {
+                    x: self.x + vector.x,
+                    y: self.y + vector.y,
+                }
+            }
+        }
+
+        impl PartialEq for Point {
+            fn eq(&self, other: &Self) -> bool {
+                self.x == other.x && self.y == other.y
+            }
+        }
+
+        impl Eq for Point {}
+
+        impl PartialOrd for Point {
+            fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+                if self.y == other.y {
+                    self.x.partial_cmp(&other.x)
+                } else {
+                    self.y.partial_cmp(&other.y)
+                }
+            }
+        }
+
+        impl Ord for Point {
+            fn cmp(&self, other: &Self) -> Ordering {
+                self.partial_cmp(other).unwrap()
+            }
+        }
+
+        impl Hash for Point {
+            fn hash<H: Hasher>(&self, state: &mut H) {
+                let x_hash = (self.x.to_bits() as u64).rotate_left(16);
+                let y_hash = self.y.to_bits() as u64;
+                state.write_u64(x_hash ^ y_hash);
+            }
+        }
+
+        impl fmt::Display for Point {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, "(x={}, y={})", self.x, self.y)
+            }
+        }
+
+        #[derive(Debug, Clone, Copy)]
+        pub struct Vector {
+            pub x: Coord,
+            pub y: Coord,
+        }
+
+        impl Vector {
+            pub fn new(x: Coord, y: Coord) -> Self {
+                Self { x, y }
+            }
+
+            pub fn scale(&self, factor: Coord) -> Vector {
+                Vector {
+                    x: self.x * factor,
+                    y: self.y * factor,
+                }
+            }
+        }
+
+        impl std::ops::Add<Vector> for Point {
+            type Output = Point;
+            fn add(self, other: Vector) -> Point {
+                Point {
+                    x: self.x + other.x,
+                    y: self.y + other.y,
+                }
+            }
+        }
+        impl std::ops::Sub for Point {
+            type Output = Vector;
+            fn sub(self, other: Point) -> Vector {
+                Vector {
+                    x: self.x - other.x,
+                    y: self.y - other.y,
+                }
+            }
+        }
+
+        impl std::ops::Add for Vector {
+            type Output = Vector;
+
+            fn add(self, other: Vector) -> Vector {
+                Vector {
+                    x: self.x + other.x,
+                    y: self.y + other.y,
+                }
+            }
+        }
+
+        impl std::ops::Sub for Vector {
+            type Output = Vector;
+
+            fn sub(self, other: Vector) -> Vector {
+                Vector {
+                    x: self.x - other.x,
+                    y: self.y - other.y,
+                }
+            }
+        }
+        impl std::ops::Div<Coord> for Vector {
+            type Output = Vector;
+
+            fn div(self, rhs: Coord) -> Self::Output {
+                Vector {
+                    x: self.x / rhs,
+                    y: self.y / rhs,
+                }
+            }
+        }
+
+        impl std::ops::Mul<Coord> for Vector {
+            type Output = Vector;
+
+            fn mul(self, rhs: Coord) -> Self::Output {
+                Vector {
+                    x: self.x * rhs,
+                    y: self.y * rhs,
+                }
+            }
+        }
+
+        #[derive(Debug, Clone)]
+        pub struct Segment {
+            pub top: Point,
+            pub bottom: Point,
+        }
+
+        impl Segment {
+            pub fn new(p1: Point, p2: Point) -> Self {
+                if p1 == p2 {
+                    panic!("Segment cannot have two identical points: {}", p1);
+                }
+
+                if p1 < p2 {
+                    Self {
+                        bottom: p1,
+                        top: p2,
+                    }
+                } else {
+                    Self {
+                        bottom: p2,
+                        top: p1,
+                    }
+                }
+            }
+
+            pub fn is_horizontal(&self) -> bool {
+                self.bottom.y == self.top.y
+            }
+
+            pub fn is_vertical(&self) -> bool {
+                self.bottom.x == self.top.x
+            }
+
+            pub fn point_on_line_x(&self, y: Coord) -> Coord {
+                if self.bottom.y == self.top.y {
+                    self.bottom.x
+                } else {
+                    self.bottom.x
+                        + (y - self.bottom.y)
+                            * ((self.top.x - self.bottom.x) / (self.top.y - self.bottom.y))
+                }
+            }
+
+            pub fn point_projection_factor(&self, p: Point) -> Coord {
+                let numerator = (p.x - self.top.x) * (self.bottom.x - self.top.x)
+                    + (p.y - self.top.y) * (self.bottom.y - self.top.y);
+                let denominator =
+                    (self.top.x - self.bottom.x).powi(2) + (self.top.y - self.bottom.y).powi(2);
+
+                numerator / denominator
+            }
+
+            pub fn point_on_line(&self, p: Point) -> bool {
+                if self.is_horizontal() {
+                    return self.bottom.x <= p.x && p.x <= self.top.x;
+                }
+
+                if self.is_vertical() {
+                    return self.bottom.y <= p.y && p.y <= self.top.y;
+                }
+
+                let x_coord = self.point_on_line_x(p.y);
+                self.bottom.x <= x_coord && x_coord <= self.top.x
+            }
+        }
+
+        impl fmt::Display for Segment {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, "[bottom={}, top={}]", self.bottom, self.top)
+            }
+        }
+
+        impl Hash for Segment {
+            fn hash<H: Hasher>(&self, state: &mut H) {
+                self.bottom.hash(state);
+                self.top.hash(state);
+            }
+        }
+
+        impl PartialEq for Segment {
+            fn eq(&self, other: &Self) -> bool {
+                self.bottom == other.bottom && self.top == other.top
+            }
+        }
+
+        impl Eq for Segment {}
+
+        impl PartialOrd for Segment {
+            fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+                if self.bottom == other.bottom {
+                    self.top.partial_cmp(&other.top)
+                } else {
+                    self.bottom.partial_cmp(&other.bottom)
+                }
+            }
+        }
+
+        impl Ord for Segment {
+            fn cmp(&self, other: &Self) -> Ordering {
+                self.partial_cmp(other).unwrap()
+            }
+        }
+
+        #[derive(Debug, Clone)]
+        pub struct Triangle {
+            pub x: usize,
+            pub y: usize,
+            pub z: usize,
+        }
+
+        impl Triangle {
+            pub fn new(x: usize, y: usize, z: usize) -> Self {
+                Triangle { x, y, z }
+            }
+        }
+
+        pub fn vector_length(p1: Point, p2: Point) -> Coord {
+            ((p1.x - p2.x).powi(2) + (p1.y - p2.y).powi(2)).sqrt()
+        }
+    }
+}
