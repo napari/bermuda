0928ea54c3a4706a32a69058b18a45f226ee1eae
Grzegorz Bokota <bokota+github@gmail.com>
2025-01-21 14:16:42 +0100

    Implement edge triangulation in rust, setup repository for build packages (#1)
    This PR contains an implementation of edge triangulation in Rust. It
    looks slightly faster than C++ version (but I think that I know the
    source of at least part of the slowdown).
    
    It is much faster than numba. 
    
    
    ```python
    import h5py
    import timeit
    import os
    
    import numpy as np
    from bermuda import triangulate_path_edge as triangulate_path_edge_rust
    from PartSegCore_compiled_backend.triangulate import triangulate_path_edge_numpy as triangulate_path_edge_cpp
    from napari.layers.shapes._accelerated_triangulate import generate_2D_edge_meshes as triangulate_path_edge_python
    
    
    DATA_PATH = os.path.join(os.path.dirname(__file__), "data.h5py")
    
    
    def load_shapes_from_hdf5(filename):
        with h5py.File(filename, "r") as f:
            polygons_ = [np.array(x) for x in f["polygons"].values()]
            affine_ = f["affine"][()]
        return polygons_, affine_
    
    
    polygons, _affine = load_shapes_from_hdf5(DATA_PATH)
    
    def check_rust():
        for polygon in polygons:
            triangulate_path_edge_rust(polygon, closed=True, bevel=False)
    
    def check_cpp():
        for polygon in polygons:
            triangulate_path_edge_cpp(polygon, closed=True, bevel=False)
    
    def check_numba():
        for polygon in polygons:
            triangulate_path_edge_python(polygon, closed=True, bevel=False)
    
    
    print("Rust")
    print(timeit.timeit(check_rust, number=20))
    
    print("Cpp")
    print(timeit.timeit(check_cpp, number=20))
    
    for i in range(2):
        check_numba()
    
    print("Numba")
    print(timeit.timeit(check_numba, number=20))
    ```
    ```
    Rust
    4.016085994997411
    Cpp
    8.888506661001884
    Numba
    19.73029466400476
    ```
    
    
    [data.zip](https://github.com/user-attachments/files/18203936/data.zip)
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a new configuration for Dependabot to manage GitHub Actions
    updates.
    - Added a GitHub Actions workflow for automating the build and
    publishing of Python packages.
    - New `triangulate_path_edge` function for triangulating paths exposed
    to Python.
    - Added a comprehensive `.gitignore` file for various programming
    environments.
    - New `pyproject.toml` file for defining build system and project
    metadata for the "bermuda" package.
    
    - **Bug Fixes**
    - Enhanced pre-commit hook configurations for improved code validation
    and formatting.
    
    - **Tests**
    - Added unit tests for the `triangulate_path_edge` function to validate
    triangulation logic.
    
    - **Documentation**
    - Updated `__init__.py` to include the new `triangulate_path_edge`
    function in the public API.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: Wouter-Michiel Vierdag <w-mv@hotmail.com>
    Co-authored-by: Ashley Anderson <aganders3@gmail.com>

---
+++ crates/triangulation/src/lib.rs
@@ -0,0 +1,6 @@
+pub mod path_triangulation;
+pub mod point;
+
+pub use crate::path_triangulation::triangulate_path_edge;
+pub use crate::path_triangulation::PathTriangulation;
+pub use crate::point::Point;
