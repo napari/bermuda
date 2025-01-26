7fcd6ef3939800ec41b69db72de6570594c2460d
Grzegorz Bokota <bokota+github@gmail.com>
2025-01-26 16:05:32 +0100

    improve tests

--- crates/bermuda/tests/intersection.rs
+++ crates/bermuda/tests/intersection.rs
@@ -3,24 +3,33 @@ use triangulation::intersection;
 use triangulation::point::Point;
 
 #[rstest]
-#[case(0.0, 0.0, 0.5, 0.5, 1.0, 1.0, true)]
-#[case(0.0, 0.0, 0.0, 0.5, 0.0, 1.0, true)]
-#[case(0.0, 0.0, 0.5, 0.0, 1.0, 0.0, true)]
-#[case(0.0, 0.0, 1.0, 1.0, 0.5, 0.5, false)]
-#[case(0.0, 0.0, 0.0, 1.0, 0.0, 0.5, false)]
-#[case(0.0, 0.0, 1.0, 0.0, 0.5, 0.0, false)]
+#[case(Point::new(0.0, 0.0), Point::new(0.5, 0.5), Point::new(1.0, 1.0), true)]
+#[case(Point::new(0.0, 0.0), Point::new(0.0, 0.5), Point::new(0.0, 1.0), true)]
+#[case(Point::new(0.0, 0.0), Point::new(0.5, 0.0), Point::new(1.0, 0.0), true)]
+#[case(
+    Point::new(0.0, 0.0),
+    Point::new(1.0, 1.0),
+    Point::new(0.5, 0.5),
+    false
+)]
+#[case(
+    Point::new(0.0, 0.0),
+    Point::new(0.0, 1.0),
+    Point::new(0.0, 0.5),
+    false
+)]
+#[case(
+    Point::new(0.0, 0.0),
+    Point::new(1.0, 0.0),
+    Point::new(0.5, 0.0),
+    false
+)]
 fn test_on_segment_if_collinear(
-    #[case] p_x: f32,
-    #[case] p_y: f32,
-    #[case] q_x: f32,
-    #[case] q_y: f32,
-    #[case] r_x: f32,
-    #[case] r_y: f32,
+    #[case] p: Point,
+    #[case] q: Point,
+    #[case] r: Point,
     #[case] expected: bool,
 ) {
-    let p = Point::new(p_x, p_y);
-    let q = Point::new(q_x, q_y);
-    let r = Point::new(r_x, r_y);
     assert_eq!(intersection::on_segment_if_collinear(p, q, r), expected);
 }
 
